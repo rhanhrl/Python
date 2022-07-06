5c17fcaafdad83990c01a3e9f101ba1823a43088
Raihan Herlambang <95472381+raihanhrl@users.noreply.github.com>
2022-07-06 12:26:12 +0700

    Add files via upload

---
+++ string.py
@@ -0,0 +1,54 @@
+'''
+String adalah satuan karakter
+'''
+
+print('===Penanda===')
+
+
+# \ (untuk mencantumkan ')
+
+print('hari jum\'at')
+
+print('======')
+
+# \\ (backslash)
+
+print('satuan angka\\bilangan')
+
+print('======')
+
+# \t (tab)
+
+print('biner = \t0 dan 1')
+
+print('======')
+
+# \b (backspace)
+
+print('rudi dan radi \btya')
+
+print('======')
+
+# \r \n (newline)
+
+print('koefisien a \nadalah 7')
+
+print('\rkonstansta nya adalah 18x')
+
+print('variabel nya \r\nadalah x')
+
+print('======')
+
+# raw string
+
+print(r'C\Users:Raihan')
+
+# multiline literal string & raw
+
+print('''
+saya adalah raihan
+''')
+
+print(r'''
+saya adalah saya
+''')
