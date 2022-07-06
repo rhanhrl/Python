9d8c68863178221bd336098c35419b4541291345
Raihan Herlambang <95472381+raihanhrl@users.noreply.github.com>
2022-07-06 12:25:02 +0700

    Add files via upload

---
+++ operasi komparasi.py
@@ -0,0 +1,83 @@
+# komparasi atau membandingkan
+
+# lebih besar dari (>)
+print("____ Lebih besar dari ____")
+a = 8
+b = 7
+
+masukan1 = a > 7
+print(a, '>', 7, '=', masukan1)
+masukan2 = b > 8
+print(b, '>', 8, '=', masukan2)
+
+# Kurang dari (<)
+print("____ Kurang dari ____")
+c = 10
+d = 9
+
+masukan3 = c < 9
+print(c, '<', 9, '=', masukan3)
+masukan4 = d < 10
+print(d, '<', 10, '=', masukan4)
+
+# Lebih dari sama dengan (>=)
+print("____ Lebih dari sama dengan ____")
+e = 12
+f = 11
+
+masukan5 = e >= 11
+print(e, '>=', 11, '=', masukan5)
+masukan6 = f >= 12
+print(f, '>=', 12, '=', masukan6)
+
+# Kurang dari sama dengan (<=)
+print("____ Kurang dari sama dengan ____")
+g = 14
+h = 13
+
+masukan7 = g <= 13
+print(g, '<=', 13, '=', masukan7)
+masukan8 = h <= 14
+print(h, '<=', 14, '=', masukan8)
+
+# Sama dengan (==)
+print("____ Sama dengan ____")
+i = 16
+j = 15
+
+masukan9 = i == j
+print(i, '==', j, '=', masukan9)
+masukan10 = j == i
+print(j, '==', i, '=', masukan10)
+
+# Tidak sama dengan (!=)
+print("____ Tidak sama dengan ____")
+k = 18
+l = 17
+
+masukan11 = k != l
+print(k, '!=', l, '=', masukan11)
+masukan12 = l != k
+print(l, '!=', k, '=', masukan12)
+
+# Membandingkan (is)
+print("____ Membandingkan ____")
+m = 20
+n = 19
+
+masukan13 = m is 19
+print(m, 'is', 19, '=', masukan13)
+masukan14 = n is 20
+print(n, 'is', 20, '=', masukan14)
+
+# Tidak membandingkan (is not)
+print("____ Tidak membandingkan ___")
+o = 22
+p = 21
+
+masukan15 = o is not 21
+print(o, 'is not', 21, masukan15)
+masukan16 = p is not 22
+print(p, 'is not', 22, '=', masukan16)
+
+print("==== Selesai ====")
