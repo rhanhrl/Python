ba82beaee8debef9f096a079f1dc98cb47e1f4f2
Raihan Herlambang <95472381+raihanhrl@users.noreply.github.com>
2022-07-06 12:27:23 +0700

    Add files via upload

---
+++ assignment.py
@@ -0,0 +1,112 @@
+print('/////////////ASSIGNMENT<<<<<<<<<<<')
+
+# assignment tambah
+masukan1 = 10
+print('\nmasukan 1 adalah =', masukan1)
+
+masukan1 += 10
+print("\nmasukan 1 sama dengan 10 ditambah 10 =", masukan1)
+
+print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+
+# assignment kurang
+masukan2 = 20
+print('\nmasukan 2 adalah :', masukan2)
+
+masukan2 -= 15
+print('\nmasukan 2 sama dengan 20 dikurangi 15 =', masukan2)
+
+print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+
+# assignment kali
+masukan3 = 50 
+print('\nmasukan 3 adalah :', masukan3)
+
+masukan3 *= 23
+print('\nmasukan 3 sama dengan 50 dikali 23 =', masukan3)
+
+print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+
+# assignment bagi
+masukan4 = 34
+print('\nmasukan 4 adalah =', masukan4)
+
+masukan4 /= 2
+print('\nmasukan 4 sama dengan 34 dibagi 2 =', masukan4)
+
+print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+
+# assignment pangkat atau eksponen
+masukan5 = 45
+print('\nmasukan 5 adalah :', masukan5)
+
+masukan5 **= 3
+print('\nmasukan 5 sama dengan 45 dipangkatkan 3 =', masukan5)
+
+print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+
+# assignment modulus atau bagi
+masukan6 = 30
+print('\nmasukan 6 adalah', masukan6)
+
+masukan6 %= 31
+print('\nmasukan 6 sama dengan 30 dipersenkan 31 =', masukan6)
+
+print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+
+# assignment floor division atau pembagian secara bulat
+masukan7 = 32
+print('\nmasukan 7 adalah :', masukan7)
+
+masukan7 //= 25
+print('\nmasukan 7 sama dengan 32 dibagi secara bulat 25 =', masukan7)
+
+print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+
+# assignment and
+masukan8 = True
+print('\nmasukan 8 adalah :', masukan8)
+
+masukan8 &= True
+print('\nmasukan 8 sama dengan true and true =', masukan8)
+
+masukan8 &= False
+print('\nmasukan 8 sama dengan true and false =', masukan8)
+
+print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+
+# assignment or
+masukan9 = True
+print('\nmasukan 9 adalah :', masukan9)
+
+masukan9 |= True
+print('\nmasukan 9 sama dengan true or true =', masukan9)
+
+masukan9 |= False
+print('\nmasukan 9 sama dengan true or false =', masukan9)
+
+print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+
+# assignment xor
+masukan10 = True
+print('\nmasukan 10 adalah :', masukan10)
+
+masukan10 ^= True
+print('\nmasukan 10 sama dengan true xor true =', masukan10)
+
+masukan10 ^= False
+print('\nmasukan 10 sama dengan false xor false =', masukan10)
+
+print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+
+# assignment shifting
+masukan11 = 0B00010000
+print('\nmasukan 11 adalah :', masukan11, format(masukan11, '08b'))
+
+masukan11 >>= 2
+print('\nmasukan 11 adalah 00010000 angka 1 digeser 2 kali =', masukan11, format(masukan11, '08b'))
+
+masukan11 <<= 4
+print('\nmasukan 11 sama dengan =', masukan11, format(masukan11, '08b'))
+
+print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
